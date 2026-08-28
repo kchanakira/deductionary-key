fbabfe8c5fdfeded67a9cd479532cb1a0c3a620b
Kingsley Chanakira <kingsley@chanakira.com>
2026-08-27 20:08:33 -0400

    Implemented a really, really basic grid of tiles with a picture and a name. Added images from the puzzles I've played so far.

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import './style.css'
+import tiles from './tiles.json'
+
+function renderTile(image, name) {
+  return `
+    <div class="card shadow-sm items-center hover:bg-neutral-700">
+      <img class="size-30 rounded-box" src="/src/assets/images/${image}" alt="${name}" />
+      <h2 class="card-title">${name}</h2>
+    </div>
+  `;
+}
+
+var sortedTiles = tiles.sort((a, b) => a.name.localeCompare(b.name));
+var tilesHtml = sortedTiles.reduce((accumulator, tile) => (accumulator ?? '') + renderTile(tile.image, tile.name), null);
+
+document.querySelector('#tiles').innerHTML = tilesHtml;
